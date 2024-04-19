68e554cb92082e259663be5c578f58120aa4a296
Adarshjain3011 <Adarshjain3011@gmail.com>
2024-04-19 16:00:12 +0530

    database configuration sucessfull

--- src/app/student/leave-form/page.tsx
+++ src/app/student/leave-form/page.tsx
@@ -1,34 +1,43 @@
 
 import React, { useState, ChangeEvent, FormEvent } from "react";
 
+import InputField from "@/components/auth/InputField";
+
+import UserFieldData from "@/constants/UserProfileFields";
 
 const Page = () => {
 
-  const [formaData,setFromData] = useState({
+  const [formaData, setFromData] = useState({
 
     EnrollMentNumber: "",
-    name:"",
-    phoneNumber:"",
-    Course:"",
-    RoomNumber:"",
-    FingerNumber:"",
-    FatherName:"",
-    LeaveDate:"",
-    LeaveTime:"",
-    LeaveReason:"",
-    LeaveStatus:"",
-    Number_of_days:"",
-    ReasonForLeave:"",
-    AddressDuringLeave:"",
-  
+    name: "",
+    phoneNumber: "",
+    Course: "",
+    RoomNumber: "",
+    FingerNumber: "",
+    FatherName: "",
+    LeaveDate: "",
+    LeaveTime: "",
+    LeaveReason: "",
+    LeaveStatus: "",
+    Number_of_days: "",
+    ReasonForLeave: "",
+    AddressDuringLeave: "",
+
   })
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+
+    const { name, value } = event.target;
+    setFromData((prev) => ({ ...prev, [name]: value }));
+    
+  };
 
   const submitHandler = async (e: FormEvent) => {
 
     e.preventDefault();
 
-    // Logic to handle form submission, like sending data to backend
-    console.log("Form submitted!");
+
 
   };
 
@@ -39,6 +48,29 @@ const Page = () => {
       <h1>Apply for Hostel leave </h1>
 
       <form onSubmit={submitHandler}>
+
+
+        {
+
+          UserFieldData.map((data: any) => (
+
+            <InputField
+
+              label={data?.label}
+              type={data?.type}
+              placeholder={data?.placeholder}
+              value={data?.label}
+              onChange={handleChange}
+
+            >
+            </InputField>
+
+
+          ))
+
+
+        }
+
 
 
       </form>
---
+++ src/constants/UserProfileFields.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+
+const FieldData = [
+    {
+        label: "Enrollment Number",
+        placeholder: "Enter your enrollment Number",
+        type: "text",
+    },
+    {
+        label: "Phone Number",
+        placeholder: "Enter your phone Number",
+        type:"Number",
+    },
+    {
+        label: "Course",
+        placeholder: "Enter your course",
+        type: "text",
+    },
+    {
+        label: "Room Number",
+        placeholder: "Enter your room Number",
+        type: "text",
+    },
+    {
+        label: "Finger Number",
+        placeholder: "Enter your finger Number",
+        type: "text",
+    },
+    {
+        label: "Father's Name",
+        placeholder: "Enter your father's Name",
+        type: "text",
+    },
+    {
+        label: "Leave Date",
+        placeholder: "Enter your leave Date",
+        type: "date",
+    },
+    {
+        label: "Leave Time",
+        placeholder: "Enter your leave Time",
+        type: "time",
+
+    },
+
+    {
+
+        label: "Leave Status",
+        placeholder: "Enter your leave Status",
+        type:"text"
+
+    },
+    {
+        label: "Number of days",
+        placeholder: "Enter the number of days",
+        type:"Number"
+    },
+    {
+        label: "Reason for Leave",
+        placeholder: "Enter the reason for Leave",
+        type:"text"
+    },
+    {
+        label: "Address During Leave",
+        placeholder: "Enter your address During Leave",
+        type:"text"
+        
+    },
+];
+
+
+export default FieldData;
+
+
