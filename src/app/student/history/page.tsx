bd4cdd3022e64385d1fbca9c7a760b922a94efe6
Adarshjain3011 <Adarshjain3011@gmail.com>
2024-04-19 14:11:48 +0530

    user history page created sucessfully

--- src/app/student/history/page.tsx
+++ src/app/student/history/page.tsx
@@ -3,11 +3,120 @@ import React from "react";
 const Page = () => {
 
 
-  return <div>
+  return <div className="bg-white min-h-screen w-full">
 
-      
+    <div className="relative w-[1200px] min-h-screen m-auto flex flex-col gap-3">
 
-  </div>;
+      <h1 className="text-black text-3xl font-semibold pt"> Your Histroy </h1>
+
+
+      <div className="realtive w-ull h-full bg-[#EDF6FF] rounded-md shadow-md p-3">
+
+        <div className="flex justify-between">
+
+
+          <div className="flex gap-10">
+
+            <div className="flex gap-2">
+
+              <div className="w-[30px] h-[30px] rounded-full">
+
+                <img src="" alt="" className="w-full h-full rounded-full" />
+
+              </div>
+
+              <div>
+
+                <h1 className="text-black font-semibold">Adarsh jain </h1>
+
+                <p className="text-black text-xs font-semibold">Tca2159055</p>
+
+              </div>
+
+            </div>
+
+
+
+            <p className="text-black">20/1/2024</p>
+
+            <p className="text-black">20/1/2024</p>
+
+
+            <p className="text-black">Approved</p>
+
+
+          </div>
+
+          <div className="flex gap-3">
+
+            <div className="bg-black rounded-full w-[30px] h-[30px]"></div>
+            <div className="bg-black rounded-full w-[30px] h-[30px]"></div>
+            <div className="bg-black rounded-full w-[30px] h-[30px]"></div>
+
+            <div className="bg-black rounded-full w-[30px] h-[30px]"></div>
+            <div className="bg-black rounded-full w-[30px] h-[30px]"></div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+      <div className="realtive w-ull h-full bg-[#EDF6FF] rounded-md shadow-md p-3">
+
+        <div className="flex justify-between">
+
+
+          <div className="flex gap-10">
+
+            <div className="flex gap-2">
+
+              <div className="w-[30px] h-[30px] rounded-full">
+
+                <img src="" alt="" className="w-full h-full rounded-full" />
+
+              </div>
+
+              <div>
+
+                <h1 className="text-black font-semibold">Adarsh jain </h1>
+
+                <p className="text-black text-xs font-semibold">Tca2159055</p>
+
+              </div>
+
+            </div>
+
+
+
+            <p className="text-black">20/1/2024</p>
+
+            <p className="text-black">20/1/2024</p>
+
+
+            <p className="text-black">Approved</p>
+
+
+          </div>
+
+          <div className="flex gap-3">
+
+            <div className="bg-black rounded-full w-[30px] h-[30px]"></div>
+            <div className="bg-black rounded-full w-[30px] h-[30px]"></div>
+            <div className="bg-black rounded-full w-[30px] h-[30px]"></div>
+
+            <div className="bg-black rounded-full w-[30px] h-[30px]"></div>
+            <div className="bg-black rounded-full w-[30px] h-[30px]"></div>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+  </div>
 
 };
 
--- src/app/student/leave-form/page.tsx
+++ src/app/student/leave-form/page.tsx
@@ -1,14 +1,51 @@
-import React from "react";
+
+import React, { useState, ChangeEvent, FormEvent } from "react";
+
 
 const Page = () => {
 
+  const [formaData,setFromData] = useState({
+
+    EnrollMentNumber: "",
+    name:"",
+    phoneNumber:"",
+    Course:"",
+    RoomNumber:"",
+    FingerNumber:"",
+    FatherName:"",
+    LeaveDate:"",
+    LeaveTime:"",
+    LeaveReason:"",
+    LeaveStatus:"",
+    Number_of_days:"",
+    ReasonForLeave:"",
+    AddressDuringLeave:"",
+  
+  })
+
+  const submitHandler = async (e: FormEvent) => {
+
+    e.preventDefault();
+
+    // Logic to handle form submission, like sending data to backend
+    console.log("Form submitted!");
+
+  };
+
   return <div>
 
-    <form>
+    <div>
 
-        
-    </form>
-    
+      <h1>Apply for Hostel leave </h1>
+
+      <form onSubmit={submitHandler}>
+
+
+      </form>
+
+    </div>
+
+
   </div>;
 
 };
