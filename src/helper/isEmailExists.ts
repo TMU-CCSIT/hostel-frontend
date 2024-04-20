9105e8019911a6bec86101afc694b62cbacad339
Adarshjain3011 <Adarshjain3011@gmail.com>
2024-04-20 16:13:14 +0530

    signup controller update

--- src/helper/isEmailExists.ts
+++ src/helper/isEmailExists.ts
@@ -1,15 +1,22 @@
 
-import user from "@/"
+import User from "@/models/User.model"
 
-export async function isEmailAlreadyExist(email) {
+import { dbConnection } from "@/app/config/dbConfig";
+
+dbConnection();
+
+export async function isEmailAlreadyExist(email:any) {
 
     try {
 
         return await User.findOne({ email });
 
     } catch (error) {
+
         throw new Error("Server failed to findout user by email");
+        
     }
 }
+
 
 
