a3e8c31b547e045b75aff45b75470c1139bbe827
Himanshu <himanshujain5690174@gmail.com>
2024-04-20 09:58:38 +0530

    env

--- src/components/auth/Signup.tsx
+++ src/components/auth/Signup.tsx
@@ -1,16 +1,13 @@
 "use client";
 
 // importing necessities
-import React, { useState } from "react";
+import React, { FormEvent, useState } from "react";
 import InputField from "./InputField";
-import { SignupFields } from "@/constants/fields";
+import DropDown from "./DropDown";
+import { colleges } from "@/constants/constant";
+import SignupData from "@/constants/SignupData";
 import CTCButton from "../common/CTCButton";
 
-<<<<<<< HEAD
-=======
-
-// interface 
->>>>>>> 8cdac5feb5a35ad6d936b9d6c5d43e3d209bdeec
 interface FormData {
   name: string;
   email: string;
@@ -25,8 +22,8 @@ interface FormData {
 }
 
 const Signup = () => {
-
   // hooks for reading different values
+
   const [data, setData] = useState<FormData>({
     name: "",
     email: "",
@@ -40,16 +37,10 @@ const Signup = () => {
     roomNumber: "",
   });
 
-
-  // function for data matching
   const handleChange = (e: any) => {
-
-
     setData({ ...data, [e.target.name]: e.target.value });
-
   };
 
-<<<<<<< HEAD
   const submitHandler = () => {
     console.log(data);
   };
@@ -61,7 +52,7 @@ const Signup = () => {
 
         <form className=" flex flex-col justify-center items-center">
           <div className="flex flex-col gap-7">
-            {SignupFields.map((a: any) => (
+            {SignupData.map((a: any) => (
               <InputField
                 key={a.name}
                 label={a?.label}
@@ -82,73 +73,6 @@ const Signup = () => {
           </div>
         </form>
       </div>
-=======
-
-  // dummy function to pass
-  const submitHandler = () =>{
-
-  const submitHandler = (e:any) =>{
-
-    e.preventDefault();
-
-
-    console.log(data);
-    
-  }
-
-  return (
-    // main div
-    <div className="bg-white min-h-screen text-black text-lg flex justify-center items-center p-5">
-
-      {/* inner div */}
-        <div className="bg-[#EDF6FF] flex flex-col p-8 rounded-md shadow-xl justify-center items-center gap-4 ">
-
-
-          {/* heading */}
-          <h1 className="text-2xl font-bold">SIGN UP</h1>
-
-              {/* form */}
-              <form  className=" flex flex-col justify-center items-center">
-
-                  <div className="flex flex-col gap-7">
-                    {
-
-                      // input field component
-                      SignupData.map((a: any) => (
-                        <InputField
-                          key={a.name}
-                          label={a?.label}
-                          type={a?.type}
-                          placeholder={a.placeholder}
-                          value={data[a.name as keyof FormData]}
-                          name={a?.name}
-                          onChange={handleChange}
-                        >
-                        </InputField>
-                      ))
-                    }
-
-                      <div className="flex flex-col ">
-                      {/* drop down  */}
-                      <DropDown name={colleges} label="Select College:"></DropDown>
-                      </div>
-
-                  </div>
-
-
-            
-
-              {/* button */}
-                  <div className="m-12">
-                      <CTCButton 
-                      text={"Submit"} 
-                      onClickHandler={submitHandler}
-                      ></CTCButton>
-                  </div>
-              </form>
-
-        </div>
->>>>>>> 8cdac5feb5a35ad6d936b9d6c5d43e3d209bdeec
     </div>
   );
 };
