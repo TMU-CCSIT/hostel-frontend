989d2ce09d762efbaae2c40278dbb0922244cee5
Emperor <jainchist@gmail.com>
2024-04-20 11:25:34 +0530

    models created

---
+++ src/models/form.model.ts
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose'
+
+const formSchema = new mongoose.Schema(
+    {
+        user:{
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"user"
+        },
+        from:{
+            type:Date,
+            required:true
+        },
+        to:{
+            type:Date,
+            required:true
+        },
+        reason:{
+            type:String,
+            required:true
+        },
+        addressDuringLeave:{
+            type:String,
+            required:true
+        }
+    }
+)
