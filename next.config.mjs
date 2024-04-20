c6c407863d9e83b77913db274b85bf8b2e411372
Himanshu <himanshujain5690174@gmail.com>
2024-04-20 16:39:00 +0530

    Navbar update

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['encrypted-tbn0.gstatic.com'],
+        domains: ['encrypted-tbn0.gstatic.com', 'portal2.tmu.ac.in'],
     },
 };
 
--- src/app/(faculty)/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const FacultyPage = () => {
-  return <div></div>;
-};
-
-export default FacultyPage;
