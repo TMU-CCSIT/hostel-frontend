cfea70de360db8f101810a30d0e00f0cab6649ae
jainArchit1 <architsupper@gmail.com>
2024-04-20 16:04:04 +0530

    mail file deleted

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import Image from "next/image";
-
-import Navbar from "@/components/common/Navbar";
-
-import Otp from "@/components/auth/Otp";
-import OtpMail from "@/components/auth/Otp";
-import ParentMail from "@/helper/mailTemplates/parentMail";
-
 export default function Home() {
   return <div></div>;
 }
