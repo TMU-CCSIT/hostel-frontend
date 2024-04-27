2e9fc14395079c4e7fb7a593d73a0061e4b30f35
Himanshu <himanshujain5690174@gmail.com>
2024-04-27 22:17:22 +0530

    Added loading during scanning

--- src/app/(navbar)/gatekeeper/scan-qrCode/page.tsx
+++ src/app/(navbar)/gatekeeper/scan-qrCode/page.tsx
@@ -1,31 +1,38 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Scanner } from "@yudiel/react-qr-scanner";
 import axios from "axios";
 import toast from "react-hot-toast";
-
-async function updateStateOfStudent(qrCodeString: string) {
-  try {
-    console.log("text: ", qrCodeString);
-    const res = await axios.put("/api/leave-form", { qrCodeString });
-    toast.success(res.data.message || "QrCode Scanned Successfully");
-  } catch (error: any) {
-    toast.error(error.response.data.message || "Something went wrong!");
-  }
-}
+import Loading from "@/components/common/Loading";
 
 const Page = () => {
+  const [loading, setLoading] = useState(false);
+
+  async function updateStateOfStudent(qrCodeString: string) {
+    try {
+      const res = await axios.put("/api/leave-form", { qrCodeString });
+      toast.success(res.data.message || "QrCode Scanned Successfully");
+    } catch (error: any) {
+      toast.error(error.response.data.message || "Something went wrong!");
+    } finally {
+      setLoading(false);
+    }
+  }
   return (
-    <div className="flex justify-center items-center w-full h-screen p-10">
-      <div className="relative w-[500px] h-[500px]">
-        <Scanner
-          onResult={async (text) => {
-            updateStateOfStudent(text);
-          }}
-          onError={(error) => console.log(error?.message)}
-        />
+    <>
+      {loading && <Loading />}
+      <div className="flex justify-center items-center w-full h-screen p-10">
+        <div className="relative w-[500px] h-[500px]">
+          <Scanner
+            onResult={async (text) => {
+              setLoading(true);
+              updateStateOfStudent(text);
+            }}
+            onError={(error) => console.log("Scanning Error: ", error?.message)}
+          />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
