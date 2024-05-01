4901875984606c1c240b735825c11c2072bda180
Emperor <jainchist@gmail.com>
2024-05-01 15:13:36 +0530

    created UI of principal

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,48 +12,48 @@ const DefaultPage = ["/", "/unauthorized"];
 
 export async function middleware(req: CustomNextRequest) {
 
-    // const path = req.nextUrl.pathname;
+    const path = req.nextUrl.pathname;
 
-    // let isLoggedIn = req.cookies.get("token")?.value || "";
+    let isLoggedIn = req.cookies.get("token")?.value || "";
 
-    // let decodedToken;
+    let decodedToken;
 
-    // if (isLoggedIn) {
-    //     decodedToken = await getDataFromToken(req);
-    //     req.user = decodedToken.id;
-    // }
+    if (isLoggedIn) {
+        decodedToken = await getDataFromToken(req);
+        req.user = decodedToken.id;
+    }
 
 
-    // if (isLoggedIn) {
-    //     decodedToken = await getDataFromToken(req);
-    //     console.log(decodedToken)
-    //     req.user = decodedToken.id;
-    // }
+    if (isLoggedIn) {
+        decodedToken = await getDataFromToken(req);
+        console.log(decodedToken)
+        req.user = decodedToken.id;
+    }
 
-    // if (DefaultPage.includes(path)) {
+    if (DefaultPage.includes(path)) {
 
-    //     return NextResponse.next();
+        return NextResponse.next();
 
-    // }
+    }
 
-    // const isPublicPath = PublicPaths.includes(path);
+    const isPublicPath = PublicPaths.includes(path);
 
-    // if (isLoggedIn && isPublicPath) {
-    //     return NextResponse.redirect(new URL(`/${(decodedToken.role)?.toLowerCase()}`, req.url));
-    // }
+    if (isLoggedIn && isPublicPath) {
+        return NextResponse.redirect(new URL(`/${(decodedToken.role)?.toLowerCase()}`, req.url));
+    }
 
-    // if (!isLoggedIn && !isPublicPath) {
-    //     return NextResponse.redirect(new URL('/auth/login', req.url));
-    // }
+    if (!isLoggedIn && !isPublicPath) {
+        return NextResponse.redirect(new URL('/auth/login', req.url));
+    }
 
-    // if (isLoggedIn) {
-    //     // If the user is logged in, check permission based on their role
+    if (isLoggedIn) {
+        // If the user is logged in, check permission based on their role
 
-    //     const hasPermission = checkPermission(decodedToken.role, path);
-    //     if (!hasPermission) {
-    //         return NextResponse.redirect(new URL('/unauthorized', req.url));
-    //     }
-    // }
+        const hasPermission = checkPermission(decodedToken.role, path);
+        if (!hasPermission) {
+            return NextResponse.redirect(new URL('/unauthorized', req.url));
+        }
+    }
     return NextResponse.next();
 
 }
